3c4b10a179cfcc8964eb9e5ae30a59f2240eb9cc
Anuragdec <125725130+Anuragdec@users.noreply.github.com>
2024-09-11 11:41:52 +0000

    color bg file mini project 1

---
+++ color-bg/bg_coloe/script.js
@@ -0,0 +1,48 @@
+const buttons = document.querySelectorAll(".button")
+const body = document.querySelector("body")
+
+buttons.forEach(function (button) { // yha per har ak element ke liye fucntion chalega
+    console.log(button)
+    button.addEventListener('click', function (e) {
+        console.log(e)
+        console.log(e.target)
+        if (e.target.id==="red"){
+            body.style.backgroundColor= e.target.id
+        }
+        console.log(e.target)
+        if (e.target.id==="green"){
+            body.style.backgroundColor= e.target.id
+        }
+        console.log(e.target)
+        if (e.target.id==="blue"){
+            body.style.backgroundColor= e.target.id
+        }
+        console.log(e.target)
+        if (e.target.id==="black"){
+            body.style.backgroundColor= e.target.id
+        }
+        
+        if (e.target.id==="white"){
+            body.style.backgroundColor= e.target.id
+        }
+        
+        if (e.target.id==="grey"){
+            body.style.backgroundColor= e.target.id
+        }
+        
+        if (e.target.id==="yellow"){
+            body.style.backgroundColor= e.target.id
+        }   
+        if (e.target.id==="brown"){
+            body.style.backgroundColor= e.target.id
+        }   
+        
+        
+    })
+
+});
+
+
+
+
+
